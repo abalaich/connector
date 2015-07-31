e30db7659ceb30c1d3ef4193a886b20f96e9a0c3
abalaich <abalaich@gmail.com>
2015-07-31 09:51:55 +0800

    first pass of the complete class

---
+++ connector/connector.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+
+# Constant for verify connection
+FTRACK_CONNECTED = False
+
+sys.path += ["C:/server/apps/3rdparty/ftrack-python"]
+
+os.environ['FTRACK_SERVER'] = 'https://cas.ftrackapp.com'
+os.environ['FTRACK_USERNAME'] = ''
+
+import ftrack
+
+FTRACK_CONNECTED = True
+
+
+class Connector(object):
+    """docstring for Connector"""
+    def __init__(self, user=None):
+        super(Connector, self).__init__()
+
+        self.user = user
+        self.userDetails = None
+        self.userTasks = None
+
+        if not self.user:
+            self.user = os.environ['USERNAME']
+
+    def getUser(self):
+        return self.user
+
+    def setUser(self, value):
+        self.user = value
+
+    def connect(self):
+
+        os.environ['LOGNAME'] = self.user
+        if FTRACK_CONNECTED is True:
+            print 'Connection Successful !'
+
+    def getUserDetails(self):
+
+        self.userDetails = ftrack.getUser(self.user)
+        return self.userDetails
+
+    def getUserTasks(self):
+
+        userDetail = self.getUserDetails()
+
+        self.userTasks = UserDetail.getTasks()
+        return self.userTasks
